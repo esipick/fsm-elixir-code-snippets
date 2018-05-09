5e913e4c5ca81bea4d01afcd2f9e7f5c6c54b0a7
Parker Wightman <parkerwightman@gmail.com>
2018-05-08 22:03:00 -0600

    Update invite email

--- lib/flight/email/email.ex
+++ lib/flight/email/email.ex
@@ -3,12 +3,20 @@ defmodule Flight.Email do
   use Bamboo.Phoenix, view: FlightWeb.EmailView
 
   def invitation_email(invitation) do
+    role = Flight.Accounts.get_role(invitation.role_id)
+
     new_email()
     |> to(invitation.email)
     # TODO: What email address to use here?
     |> from("noreply@randonaviation.com")
     |> subject("Welcome to Randon Aviation!")
-    |> render("invitation.html", link: invitation_link(invitation))
+    |> render(
+      "invitation.html",
+      link: invitation_link(invitation),
+      company_name: "Randon Aviation",
+      first_name: invitation.first_name,
+      role: role.slug
+    )
   end
 
   defp invitation_link(invitation) do
