7a71a6d996e2e4abf014a8e8336f5f4217839241
Cloud9 Server <ec2-user@ip-172-31-0-38.us-west-2.compute.internal>
2018-06-25 20:11:43 +0000

    Add uncommitted files.

---
+++ lib/flight_web/controllers/admin/admin_settings_controller.ex
@@ -0,0 +1,12 @@
+defmodule FlightWeb.Admin.SettingsController do
+  use FlightWeb, :controller
+
+  def index(conn, _) do
+    render(conn, "index.html")
+  end
+  
+  def edit(conn, _) do
+    render(conn, "edit.html")
+  end
+
+end
