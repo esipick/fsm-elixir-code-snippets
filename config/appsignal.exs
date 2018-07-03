305586f0a1bf3dc08f83f01fc6162ba72d290e7d
Parker Wightman <parkerwightman@gmail.com>
2018-07-03 07:52:16 -0600

    Use AppSignal environment to configure staging

--- config/appsignal.exs
+++ config/appsignal.exs
@@ -4,4 +4,4 @@ config :appsignal, :config,
   active: Mix.env() not in [:dev, :test],
   name: "randon-aviation",
   push_api_key: "e6c5ebe7-ea51-4eb8-bcfa-b23071f09340",
-  env: Mix.env()
+  env: System.get_env("APPSIGNAL_ENV") || Mix.env()
