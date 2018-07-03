6bf1c71b4ea62b670f388df9c6670a971df9c031
Parker Wightman <parkerwightman@gmail.com>
2018-07-03 07:49:28 -0600

    Fix app name

--- config/appsignal.exs
+++ config/appsignal.exs
@@ -2,6 +2,6 @@ use Mix.Config
 
 config :appsignal, :config,
   active: Mix.env() not in [:dev, :test],
-  name: "FSM",
+  name: "randon-aviation",
   push_api_key: "e6c5ebe7-ea51-4eb8-bcfa-b23071f09340",
   env: Mix.env()
