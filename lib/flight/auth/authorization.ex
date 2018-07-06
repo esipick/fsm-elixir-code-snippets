49aeea3094fab56a73ea442adf450cc4f66ac8cf
Parker Wightman <parkerwightman@gmail.com>
2018-07-06 08:06:05 -0600

    Add modify permission to admins

--- lib/flight/auth/authorization.ex
+++ lib/flight/auth/authorization.ex
@@ -58,6 +58,7 @@ defmodule Flight.Auth.Authorization do
       permission_slug(:transaction_creator, :view, :all),
       permission_slug(:transaction_user, :modify, :all),
       permission_slug(:transaction, :request, :all),
+      permission_slug(:transaction, :modify, :all),
       permission_slug(:transaction_user, :view, :all)
     ])
   end
