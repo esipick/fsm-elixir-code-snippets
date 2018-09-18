30eadc97807d5ec89c81ca79f06597e30ef286bb
Parker Wightman <parkerwightman@gmail.com>
2018-09-18 14:30:26 -0600

    Add archived flag migration to aircraft

---
+++ priv/repo/migrations/20180918164329_add_archived_to_aircraft.exs
@@ -0,0 +1,9 @@
+defmodule Flight.Repo.Migrations.AddArchivedToAircraft do
+  use Ecto.Migration
+
+  def change do
+    alter table(:aircrafts) do
+      add :archived, :boolean, default: false, null: false
+    end
+  end
+end
