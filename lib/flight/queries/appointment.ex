082ad64646f8759199503a469ca62f4a69373db2
Slava Kisel <slava@skdev.info>
2020-03-30 13:46:35 +0300

    Fix billable appointments query

--- lib/flight/queries/appointment.ex
+++ lib/flight/queries/appointment.ex
@@ -13,7 +13,7 @@ defmodule Flight.Queries.Appointment do
 
     from(i in Appointment, where: i.end_at <= ^datetime, order_by: [desc: i.end_at])
     |> where([i], i.archived == false)
-    |> where([i], i.status == "paid")
+    |> where([i], i.status == "pending")
     |> SchoolScope.scope_query(school_context)
     |> pass_unless(params["user_id"], &where(&1, [t], t.user_id == ^params["user_id"]))
     |> Repo.all()
