94d354c45537fb162bec6d1e862617b9115b5f07
imahmad <idrees@skyelectric.com>
2020-08-14 17:07:05 +0500

    added pub key for iOS client

--- lib/flight/billing/stripe_single_payment.ex
+++ lib/flight/billing/stripe_single_payment.ex
@@ -40,6 +40,8 @@ defmodule Flight.StripeSinglePayment do
 
         with %{stripe_account_id: acc_id} <- Billing.get_stripe_account_by_school_id(school_id),
             {:ok, %{id: id, client_secret: secret}} <- create_payment_intent(acc_id, total_amount) do
+                pub_key = FlightWeb.StripeHelper.stripe_key()
+
                 {:ok, %{id: id, session_id: secret, connect_account: acc_id, pub_key: pub_key}}
 
         else
