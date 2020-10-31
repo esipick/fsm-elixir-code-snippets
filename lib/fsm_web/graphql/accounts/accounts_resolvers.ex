b53c444e2a93ea3b24522a87aa14021872e67f8d
Hassaan <hassaanahmad91@gmail.com>
2020-11-01 01:14:38 +0500

    Updated - Users

--- lib/fsm_web/graphql/accounts/accounts_resolvers.ex
+++ lib/fsm_web/graphql/accounts/accounts_resolvers.ex
@@ -7,5 +7,11 @@ defmodule FsmWeb.GraphQL.Accounts.AccountsResolvers do
       Logger.info fn -> "resolution: #{}" end
       Accounts.api_login(%{"email" => email, "password"=> password} )
     end
+
+    def all_users(parent, _params, resolution) do
+      Logger.info fn -> "resolution: #{inspect resolution}" end
+      Logger.info fn -> "parent: #{inspect parent}" end
+      {:ok, []}
+    end
   end
   
