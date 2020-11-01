a33e7501f7d765b7e328fe524efa258e60d2e17d
amalsultan999 <amal.sultan@skyelectric.com>
2020-11-01 16:23:52 +0500

    fetch transactions on the basis of roles

--- lib/fsm_web/graphql/transactions/transactions_resolvers.ex
+++ lib/fsm_web/graphql/transactions/transactions_resolvers.ex
@@ -2,8 +2,12 @@ defmodule FsmWeb.GraphQL.Transactions.TransactionsResolvers do
     alias Fsm.Transactions
     require Logger
 
-    def get_all_transactions(_parent, _args, _context) do
-        Transactions.get_all_transactions()
+    def get_all_transactions(parent, args, %{context: %{current_user: %{school_id: school_id, roles: roles, id: id}}}=context) do 
+        if "admin" in roles or "dispatcher" in roles do
+            Transactions.get_all_transactions()
+        else
+            Transactions.get_transactions_by_user_id(id)
+        end
     end
 end
     
