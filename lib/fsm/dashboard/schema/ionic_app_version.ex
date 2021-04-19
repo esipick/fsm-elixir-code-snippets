0a31ee9e347a173b0d3f7d1ee1f25578e6f3a64a
Hassaan <hassaanahmad91@gmail.com>
2021-04-20 04:49:55 +0500

    Updated android and ios app versions in schema

--- lib/fsm/dashboard/schema/ionic_app_version.ex
+++ lib/fsm/dashboard/schema/ionic_app_version.ex
@@ -5,13 +5,17 @@ defmodule Fsm.IonicAppVersion do
   schema "ionic_app_versions" do
     field(:version, :string)
     field(:int_version, :integer)
+    field(:android_version, :string)
+    field(:android_int_version, :integer)
+    field(:ios_version, :string)
+    field(:ios_int_version, :integer)
     timestamps([inserted_at: :created_at])
   end
 
   @doc false
   def changeset(role, attrs) do
     role
-    |> cast(attrs, [:version, :int_version])
-    |> validate_required([:version, :int_version])
+    |> cast(attrs, [:version, :int_version, :android_version, :android_int_version, :ios_version, :ios_int_version])
+    |> validate_required([:version, :int_version, :android_version, :android_int_version, :ios_version, :ios_int_version])
   end
 end
