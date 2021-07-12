576993395b6f497f3f7274277e7173da962ad8a8
Mudassar Ali <mudassir.ali@team.esipick.com>
2021-07-12 21:58:29 +0500

    fix return bug

--- lib/fsm_web/graphql/attachments/attachment_resolvers.ex
+++ lib/fsm_web/graphql/attachments/attachment_resolvers.ex
@@ -9,8 +9,7 @@ defmodule FsmWeb.GraphQL.Attachments.AttachmentsResolvers do
   Returns a presigned url for file uploads
   """
   def get_presigned_url(_parent, %{inspection_id: id, file_ext: file_ext}, %{context: %{current_user: current_user}}) do
-    url = AttachmentUploader.get_presigned_url(id, file_ext)
-    {:ok, url}
+     AttachmentUploader.get_presigned_url(id, file_ext)
   end
 
   def get_presigned_url(_parent, _args, _context), do: @not_authenticated
