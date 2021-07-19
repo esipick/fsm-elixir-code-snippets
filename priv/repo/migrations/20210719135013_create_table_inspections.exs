5aa86b33a0d0584f656edc02bf9866342b339656
Mudassar Ali <mudassir.ali@team.esipick.com>
2021-07-19 19:11:18 +0500

    add new inspections table

---
+++ priv/repo/migrations/20210719135013_create_table_inspections.exs
@@ -0,0 +1,32 @@
+defmodule Flight.Repo.Migrations.CreateTableInspections do
+  use Ecto.Schema
+  import Ecto.Changeset
+  import Ecto.SoftDelete.Schema
+
+  def change do
+    drop_if_exists(table("inspections"))
+
+    DateTachEnum.create_type()
+
+    create table(:inspections) do
+      add(:name, :string)
+      add(:type, :string)
+      add(:update, :boolean, default: false)
+      add(:is_completed, :boolean, default: false)
+      add(:note, :string)
+      add(:is_repeated, :boolean)
+      add(:repeat_every_days, :integer)
+      add(:date_tach, DateTachEnum.type())
+      add(:is_notified, :boolean, default: false)
+      add(:is_email_notified, :boolean)
+      add(:is_system_defined, :boolean)
+      add(:completed_at, :naive_datetime)
+
+      add(:aircraft_id, references(:aircrafts, on_delete: :nothing))
+      add(:engine_id, references(:aircraft_engines, on_delete: :nothing))
+
+      soft_delete_columns()
+      timestamps()
+    end
+  end
+end
