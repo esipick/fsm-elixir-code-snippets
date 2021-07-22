1e07f8f9ceab5d66da44c38ddfe5aceea8a27bec
Mudassar Ali <mudassir.ali@team.esipick.com>
2021-07-22 21:26:06 +0500

    add resolvers for inspections

--- lib/fsm_web/graphql/aircrafts/inspections_resolvers.ex
+++ lib/fsm_web/graphql/aircrafts/inspections_resolvers.ex
@@ -1,136 +1,87 @@
-defmodule FsmWeb.GraphQL.Aircrafts.InspectionsTypes do
-  use Absinthe.Schema.Notation
+defmodule FsmWeb.GraphQL.Aircrafts.InspectionsResolvers do
+  use FsmWeb.GraphQL.Errors
+  alias Fsm.Inspections
+  alias FsmWeb.GraphQL.EctoHelpers
+  require Logger
 
-  alias FsmWeb.GraphQL.Middleware
-  alias FsmWeb.GraphQL.Aircrafts.InspectionsResolvers
+  @doc """
+  Get active aircraft inspections
+  """
+  def get_inspections(_parent, %{aircraft_id: aircraft_id} = args, %{
+        context: %{current_user: %{id: user_id}}
+      }) do
+    page = Map.get(args, :page)
+    per_page = Map.get(args, :per_page)
+    filter = Map.get(args, :filter) || %{}
 
-  enum(:field_type, values: [:int, :string, :date, :float])
-  enum(:inspection_type, values: ["IFR", "VFR"])
-  enum(:date_tach, values: [:date, :tach])
-  enum(:inspection_sort_fields, values: [:updated, :is_completed, :name, :completed_at])
+    filter =
+      case Map.has_key?(filter, :completed) do
+        true ->
+          filter
 
-  # Inspections Queries
+        false ->
+          filter = filter |> Map.put(:completed, false)
+      end
 
-  object :inspections_queries do
-    @desc "Get inspections by aircraft_id"
-    field :get_inspections, list_of(:inspection) do
-      arg(:aircraft_id, non_null(:id))
-      arg(:page, :integer, default_value: 1)
-      arg(:per_page, :integer, default_value: 100)
-      arg(:filter, :inspection_filter)
-      resolve(&InspectionsResolvers.get_inspections/3)
-    end
+    case Inspections.get_inspections(user_id, aircraft_id, page, per_page, filter) do
+      nil ->
+        @not_found
 
-    @desc "Get inspection by inspection_id"
-    field :get_inspection, :inspection do
-      arg(:inspection_id, non_null(:id))
-      resolve(&InspectionsResolvers.get_inspection/3)
-    end
-  end
-
-  # Inspections Mutations
-
-  object :inspections_mutations do
-    @desc "Add inspection"
-    field :add_inspection, :inspection do
-      arg(:inspection_input, non_null(:inspection_input))
-      resolve(&InspectionsResolvers.add_inspection/3)
-    end
-
-    @desc "Update inspection"
-    field :update_inspection, :boolean do
-      arg(:id, non_null(:id))
-      arg(:data, list_of(non_null(:inspection_keyval)))
-      resolve(&InspectionsResolvers.update_inspection/3)
-    end
-
-    @desc "Complete inspection"
-    field :complete_inspection, :inspection do
-      arg(:completion_input, non_null(:completion_input))
-      resolve(&InspectionsResolvers.complete_inspection/3)
-    end
-
-    @desc "Delete inspection"
-    field :delete_inspection, :inspection do
-      arg(:id, non_null(:id))
-      resolve(&InspectionsResolvers.delete_inspection/3)
+      inspections ->
+        {:ok, inspections}
     end
   end
 
-  # Inspection Types
+  def get_inspections(_, _, _), do: @not_authenticated
 
-  object :inspection do
-    field(:id, :integer)
-    field(:name, :string)
-    field(:type, :inspection_type)
-    field(:updated, :boolean)
-    field(:is_repeated, :boolean)
-    field(:is_completed, :boolean)
-    field(:is_system_defined, :boolean)
-    field(:repeat_every_days, :integer)
-    field(:date_tach, :date_tach)
-    field(:completed_at, :naive_datetime)
-    field(:inspection_data, list_of(non_null(:inspection_field)))
-    field(:attachments, list_of(non_null(:attachment)))
+  @doc """
+  Get inspection
+  """
+  def get_inspection(_parent, %{inspection_id: inspection_id} = args, %{
+        context: %{current_user: %{id: user_id}}
+      }) do
+    case Inspections.get_inspection_by_inspection_id(inspection_id) do
+      nil ->
+        @not_found
+
+      inspection ->
+        {:ok, inspection}
+    end
   end
 
-  object :inspection_field do
-    field(:name, :string)
-    field(:value, :string)
-    field(:type, :string)
-    field(:class_name, :string)
-    field(:sort, :integer)
+  def get_inspection(_, _, _), do: @not_authenticated
+
+  @doc """
+  Update inspection data
+  """
+  def update_inspection(_parent, %{id: id, data: data}, %{context: %{current_user: current_user}}) do
+    case Inspections.update_inspection(current_user.id, id, data) do
+      {:ok, _updated_record} ->
+        {:ok, true}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
   end
 
-  input_object :inspection_input do
-    field(:name, :string)
-    field(:type, :inspection_type)
-    field(:updated, :boolean)
-    field(:aircraft_id, :integer)
-    field(:date_tach, :date_tach)
-    field(:inspection_data, list_of(non_null(:inspection_data_input)))
+  def update_inspection(_parent, _args, _context), do: @not_authenticated
+
+  def complete_inspection(_parent, args, %{context: %{current_user: current_user}}) do
+    EctoHelpers.action_wrapped(fn ->
+      Inspections.complete_inspection(current_user.id, args.completion_input)
+    end)
   end
 
-  input_object :inspection_data_input do
-    field(:name, :string)
-    field(:value, :string)
-    field(:type, :field_type)
-    field(:class_name, :string)
-    field(:sort, :integer)
+  def complete_inspection(_parent, _args, _context), do: @not_authenticated
+
+  @doc """
+  add inspection data
+  """
+  def add_inspection(_parent, args, %{context: %{current_user: current_user}}) do
+    EctoHelpers.action_wrapped(fn ->
+      Inspections.add_inspection(args.inspection_input, current_user)
+    end)
   end
 
-  input_object :inspection_filter do
-    field(:name, :string)
-    field(:type, :string)
-    field(:note, :string)
-    field(:updated, :boolean)
-    field(:aircraft_id, :integer)
-    field(:sort_order, :order_by)
-    field(:sort_field, :inspection_sort_fields)
-    field(:completed, :boolean)
-  end
-
-  input_object :inspection_keyval do
-    field(:name, :string)
-    field(:value, :string)
-  end
-
-  input_object :completion_input do
-    field(:inspection_id, non_null(:id))
-    field(:note, :string)
-    field(:next_inspection, :naive_datetime)
-    field(:is_repeated, :boolean)
-    field(:repeat_every_days, :integer)
-    field(:tach_hours, :float)
-  end
-
-  input_object :inspection_input do
-    field :name, :string
-    field :type, :inspection_type
-    field :updated, :boolean
-    field :aircraft_id, :integer
-    field :date_tach, :date_tach
-    field :inspection_data, list_of(non_null(:inspection_data_input))
-   end
-
+  def add_inspection(_parent, _args, _context), do: @not_authenticated
 end
