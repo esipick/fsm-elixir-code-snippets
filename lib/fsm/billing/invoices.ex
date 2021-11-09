e2be74fe7353f767fc3d624ff6cafeb993e7560e
Ahmad Wali <ahmad.wali@esipick.com>
2021-11-09 17:57:08 +0500

    Cron should create a single course invoice per month. #892

--- lib/fsm/billing/invoices.ex
+++ lib/fsm/billing/invoices.ex
@@ -1,11 +1,22 @@
 defmodule Fsm.Billing.Invoices do
   import Ecto.Query, warn: false
-
+  alias Flight.Billing.{
+    Invoice
+    }
   alias Flight.Repo
   alias Fsm.Billing.InvoicesQueries
 
   def list_custom_line_items(conn) do
     InvoicesQueries.list_custom_line_items_query(conn)
     |> Repo.all
+  end
+  def getCurrentMonthCourseAdminInvoice(school_id) do
+    from(
+      i in Invoice,
+      where: i.school_id == ^school_id,
+      order_by: [desc: i.inserted_at],
+      limit: 1
+    )
+    |> Flight.Repo.one()
   end
 end
