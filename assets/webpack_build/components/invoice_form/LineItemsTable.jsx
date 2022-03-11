a79a2148c91dd7a919d1feb187040530a0ef4980
mudssrali <mudassaralics007@gmail.com>
2022-03-12 00:41:38 +0500

    chore: show total amount for maintenance invoice

--- assets/webpack_build/components/invoice_form/LineItemsTable.jsx
+++ assets/webpack_build/components/invoice_form/LineItemsTable.jsx
@@ -134,42 +134,36 @@ class LineItemsTable extends Component {
               </td>
             }
           </tr>
-         {
-           !isMaintenanceInvoice(appointment, line_items) && (
-             <>
-              <tr>
-                <td colSpan="5" className="text-right">
-                  Total excl. taxes:
-                </td>
-                <td colSpan="2" className={total < 0 ? 'deductible' : ''}>
-                  ${(total / 100).toFixed(2)}
-                </td>
-              </tr>
-              <tr>
-                <td colSpan="5" className="text-right">
-                  Sales Tax %:
-                </td>
-                <td colSpan="2">{sales_tax}</td>
-              </tr>
-              <tr>
-                <td colSpan="5" className="text-right">
-                  Total tax:
-                </td>
-                <td colSpan="2" className={total_tax < 0 ? 'deductible' : ''}>
-                  ${(total_tax / 100).toFixed(2)}
-                </td>
-              </tr>
-              <tr>
-                <td colSpan="5" className="text-right">
-                  Total with Tax:
-                </td>
-                <td colSpan="2" className={total_amount_due < 0 ? 'deductible' : ''}>
-                  ${(total_amount_due / 100).toFixed(2)}
-                </td>
-              </tr>
-             </>
-           )
-         }
+          <tr>
+            <td colSpan="5" className="text-right">
+              Total excl. taxes:
+            </td>
+            <td colSpan="2" className={total < 0 ? 'deductible' : ''}>
+              ${(total / 100).toFixed(2)}
+            </td>
+          </tr>
+          <tr>
+            <td colSpan="5" className="text-right">
+              Sales Tax %:
+            </td>
+            <td colSpan="2">{sales_tax}</td>
+          </tr>
+          <tr>
+            <td colSpan="5" className="text-right">
+              Total tax:
+            </td>
+            <td colSpan="2" className={total_tax < 0 ? 'deductible' : ''}>
+              ${(total_tax / 100).toFixed(2)}
+            </td>
+          </tr>
+          <tr>
+            <td colSpan="5" className="text-right">
+              Total with Tax:
+            </td>
+            <td colSpan="2" className={total_amount_due < 0 ? 'deductible' : ''}>
+              ${(total_amount_due / 100).toFixed(2)}
+            </td>
+          </tr>
         </tbody>
       </table>
     )
