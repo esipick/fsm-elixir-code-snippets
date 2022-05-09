0ee3bc91d16c7c07e8edd31b8781235a06570aa4
Ahmad Wali <ahmad.wali@esipick.com>
2022-05-09 12:16:08 +0500

    Students can schedule and bill the simulator for solo flight. Right now it requires an instructor, but apparently we have a lot of students that are going solo in the sim. #1177

--- lib/fsm/scheduling/scheduling.ex
+++ lib/fsm/scheduling/scheduling.ex
@@ -240,9 +240,20 @@ defmodule Fsm.Scheduling do
 
       {:ok, _} = apply_action(changeset, :insert)
 
-      # |> utc_to_walltime(school.timezone)
-      start_at = get_field(changeset, :inst_start_at) # |> utc_to_walltime(school.timezone)
-      end_at = get_field(changeset, :inst_end_at) # |> utc_to_walltime(school.timezone)
+      start_at = case get_field(changeset, :inst_start_at) do
+        nil->
+          get_field(changeset, :start_at)
+        _->
+          get_field(changeset, :inst_start_at)
+      end
+
+      end_at = case get_field(changeset, :inst_end_at) do
+        nil->
+          get_field(changeset, :end_at)
+        _->
+          get_field(changeset, :inst_end_at)
+      end
+
       user_id = get_field(changeset, :user_id)
       instructor_user_id = get_field(changeset, :instructor_user_id)
       mechanic_user_id = get_field(changeset, :mechanic_user_id)
